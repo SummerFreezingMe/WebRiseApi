59e2b5dfde22b668f1cad606c68a504432ff2f64
SummerFreezingMe <m.byckoff2015@yandex.ru>
2025-05-13 23:57:21 +0300

    Initial commit

---
+++ src/main/java/com/example/webriseapi/WebRiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.webriseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRiseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRiseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webriseapi/WebRiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webriseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRiseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
